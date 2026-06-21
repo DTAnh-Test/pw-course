95dc134c298bc6964a10d14f33b34dc7a1f224cc
Tuan Anh <dangtuananh94.it@gmail.com>
2026-06-21 18:28:04 +0700

    chore: Add testcase @ACC_002

--- tests/students-submission/tuan-anh/lesson-8/account.spec.ts
+++ tests/students-submission/tuan-anh/lesson-8/account.spec.ts
@@ -6,10 +6,10 @@ const accountAdmin = {
 };
 
 const accountUsers = {
-    username: 'K18_Tuan Anh',
+    username: 'K23_Tuan Anh',
     email: 'abc321312@gmail.com',
     password: '&V9XK3ItwzY*i!EKQ*Pax1ZR',
-    firstName: 'Dang',
+    firstName: 'K23',
     lastName: 'Anh'
 }
 
@@ -55,12 +55,12 @@ test.describe('ACCOUNT - Account', async () => {
         await expect(page.locator('//div[@id="adminmenuwrap"]//div[text()="Dashboard"]')).toBeVisible();
         await expect(page.locator('//div[@id="adminmenuwrap"]//div[text()="Profile"]')).toBeVisible();
         await expect(page.locator('//div[@id="adminmenuwrap"]//div[@class="wp-menu-name" and text()="Posts"]')).toBeVisible();
-        await expect(page.locator('//div[@id="adminmenuwrap"]//div[text()="Plugins "]')).toBeHidden();
         await expect(page.locator('//div[@id="adminmenuwrap"]//div[text()="Media"]')).toBeVisible();
         await expect(page.locator('//div[@id="adminmenuwrap"]//div[text()="Pages"]')).toBeVisible();
         await expect(page.locator('//div[@id="adminmenuwrap"]//div[text()="Comments "]')).toBeVisible();
         await expect(page.locator('//div[@id="adminmenuwrap"]//div[text()="Tools"]')).toBeVisible();
 
+        await expect(page.locator('//div[@id="adminmenuwrap"]//div[text()="Plugins "]')).toBeHidden();
         await expect(page.locator('//div[@id="adminmenuwrap"]//div[text()="Appearance"]')).toBeHidden();
         await expect(page.locator('//div[@id="adminmenuwrap"]//div[text()="Users"]')).toBeHidden();
 
@@ -93,6 +93,78 @@ test.describe('ACCOUNT - Account', async () => {
         });
 
         await expect(page.locator('//td[@class="colspanchange"]')).toHaveText('No users found.');
+    });
 
+
+    test('@ACC_002: Create account with subscriber  permission', async ({ page }) => {
+        await test.step('Go to Users page', async () => {
+            await page.locator('//li[@class="wp-has-submenu wp-not-current-submenu menu-top menu-icon-users"]/a').click();
+        });
+
+        await expect(page.locator('//h1[@class="wp-heading-inline"]')).toBeVisible();
+        await expect(page.locator('//a[@class="page-title-action"]')).toBeEnabled();
+
+        await test.step('Create new user with editor permission', async () => {
+            await page.locator('//a[@class="page-title-action"]').click();
+            await page.locator('//input[@name="user_login"]').fill(accountUsers.username);
+            await page.locator('//input[@name="email"]').fill(accountUsers.email);
+            await page.locator('//input[@name="first_name"]').fill(accountUsers.firstName);
+            await page.locator('//input[@name="last_name"]').fill(accountUsers.lastName);
+            await page.locator('//input[@id="pass1"]').fill(accountUsers.password);
+            await page.locator('//select[@name="role"]').selectOption('subscriber');
+            await page.locator('//input[@name="createuser"]').click();
+        });
+
+        await test.step('Logout admin account and login user account', async () => {
+            await page.locator('//img[@class="avatar avatar-26 photo"]').hover();
+            await expect(page.locator('//img[@class="avatar avatar-26 photo"]/parent::a/following-sibling::div//a[text()="Log Out"]')).toBeVisible();
+            await page.locator('//img[@class="avatar avatar-26 photo"]/parent::a/following-sibling::div//a[text()="Log Out"]').click();
+
+            await page.locator('//input[@id="user_login"]').fill(accountUsers.username);
+            await page.locator('//input[@id="user_pass"]').fill(accountUsers.password);
+            await page.locator('//input[@id="wp-submit"]').click();
+        });
+
+        await expect(page.locator('//div[@id="adminmenuwrap"]//div[text()="Dashboard"]')).toBeVisible();
+        await expect(page.locator('//div[@id="adminmenuwrap"]//div[text()="Profile"]')).toBeVisible();
+
+        await expect(page.locator('//div[@id="adminmenuwrap"]//div[@class="wp-menu-name" and text()="Posts"]')).toBeHidden();
+        await expect(page.locator('//div[@id="adminmenuwrap"]//div[text()="Media"]')).toBeHidden();
+        await expect(page.locator('//div[@id="adminmenuwrap"]//div[text()="Pages"]')).toBeHidden();
+        await expect(page.locator('//div[@id="adminmenuwrap"]//div[text()="Comments "]')).toBeHidden();
+        await expect(page.locator('//div[@id="adminmenuwrap"]//div[text()="Tools"]')).toBeHidden();
+        await expect(page.locator('//div[@id="adminmenuwrap"]//div[text()="Plugins "]')).toBeHidden();
+        await expect(page.locator('//div[@id="adminmenuwrap"]//div[text()="Appearance"]')).toBeHidden();
+        await expect(page.locator('//div[@id="adminmenuwrap"]//div[text()="Users"]')).toBeHidden();
+
+        await test.step('Login admin account and delete user account', async () => {
+            await page.locator('//img[@class="avatar avatar-26 photo"]').hover();
+            await expect(page.locator('//img[@class="avatar avatar-26 photo"]/parent::a/following-sibling::div//a[text()="Log Out"]')).toBeVisible();
+            await page.locator('//img[@class="avatar avatar-26 photo"]/parent::a/following-sibling::div//a[text()="Log Out"]').click();
+
+            await page.locator('//input[@id="user_login"]').fill(accountAdmin.username);
+            await page.locator('//input[@id="user_pass"]').fill(accountAdmin.password);
+            await page.locator('//input[@id="wp-submit"]').click();
+
+            await page.locator('//li[@class="wp-has-submenu wp-not-current-submenu menu-top menu-icon-users"]/a').click();
+            await page.locator('//input[@type="search"]').fill(accountUsers.username);
+            await page.locator('//input[@id="search-submit"]').click();
+
+            await page.locator('//thead//input[@id="cb-select-all-1"]').click();
+            await page.locator('//select[@name="action"]').selectOption('delete');
+            await page.locator('//div[@class="tablenav top"]//input[@id="doaction"]').click();
+
+            if (await page.locator('//label[text()="Delete all content."]//preceding-sibling::input').isVisible() === true) {
+                await page.locator('//label[text()="Delete all content."]//preceding-sibling::input').check();
+            }
+            await page.locator('//input[@id="submit"]').click();
+
+
+            await page.locator('//input[@type="search"]').fill(accountUsers.username);
+            await page.locator('//input[@id="search-submit"]').click();
+
+        });
+
+        await expect(page.locator('//td[@class="colspanchange"]')).toHaveText('No users found.');
     });
 });
